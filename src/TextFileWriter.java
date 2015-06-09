fab30ebf3a09a6701f5089dec08dde2f967c5b38
Andrew Burke <andrew_burke@ryecountryday.org>
2015-06-09 14:24:17 -0400

    TextFileWriter done

--- src/TextFileWriter.java
+++ src/TextFileWriter.java
@@ -1,10 +1,41 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class TextFileWriter
 {
+	public TextFileWriter()
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Please enter the filename to write: ");
+		String input = keyboard.nextLine();
+
+		try
+		{
+			//open the output stream
+			PrintWriter output = new PrintWriter(input);
+
+			while (!input.equals("_done"))
+			{
+				System.out.print("Please enter some text: ");
+				input = keyboard.nextLine();
+				//send things to the output stream
+				output.println(input);
+			}
+
+			System.out.println("Finished!");
+			
+			//close the stream!
+			output.close();
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		keyboard.close();
+	}
 
 	public static void main(String[] args)
 	{
-
+		new TextFileWriter();
 	}
 
 }
