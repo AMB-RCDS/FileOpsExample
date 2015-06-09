d5bf208faec9069f7a345a1e00c0ad1b3459b4e9
Andrew Burke <andrew_burke@ryecountryday.org>
2015-06-09 13:40:33 -0400

    TextFileLoader complete

--- src/TextFileLoader.java
+++ src/TextFileLoader.java
@@ -1,5 +1,46 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TextFileLoader
 {
+	public TextFileLoader()
+	{
+		Scanner keyboard, textLoader;
+		
+		//get input from the CONSOLE
+		keyboard = new Scanner(System.in);
+		System.out.print("Please enter the name of a file to load: ");
+		String fName = keyboard.nextLine();
+		keyboard.close();
+		
+		//get input from the FILE
+		try
+		{
+			textLoader = new Scanner(new File(fName));
 
+			ArrayList<String> rawData = new ArrayList<>();
+			
+			//as long as there are lines left to read...
+			while (textLoader.hasNext())
+				//read a line, store it in rawData
+				rawData.add(textLoader.nextLine());
+
+			//don't forget to close the Scanner!  This protects the file!
+			textLoader.close();
+
+			//print out all the information loaded
+			for (int i = 0; i < rawData.size(); i++)
+				System.out.println("Line " + i + ": " + rawData.get(i));
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		new TextFileLoader();
+	}
 }
