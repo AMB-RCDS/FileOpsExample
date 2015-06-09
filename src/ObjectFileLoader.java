c700e69020e81cfbefeda2acebb157e5a82c8aec
Andrew Burke <andrew_burke@ryecountryday.org>
2015-06-09 13:34:27 -0400

    Initial - empty files

---
+++ src/ObjectFileLoader.java
@@ -0,0 +1,10 @@
+
+public class ObjectFileLoader
+{
+
+	public static void main(String[] args)
+	{
+
+	}
+
+}
---
+++ src/TextFileWriter.java
@@ -0,0 +1,10 @@
+
+public class TextFileWriter
+{
+
+	public static void main(String[] args)
+	{
+
+	}
+
+}
